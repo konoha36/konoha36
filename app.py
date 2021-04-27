8b2fe5e16af275606f8b27ebd6f325752732aff7
konoha36 <cooper_palm@outlook.com>
2021-04-27 16:34:14 +0700

    update

--- app.py
+++ app.py
@@ -1,1 +1,29 @@
-print("testapp")
+name = input("What is your name? ")
+
+print("Hello "+ name)
+
+
+birth_year = input("Enter your birthday: ")
+age = 2020 - int(birth_year)
+price = 19.95
+first_name = 'Mosh'
+is_online = True
+
+
+print("age= "+str(age))
+
+int()
+float()
+bool()
+str()
+
+first = float(input("First: "))
+second = float(input("Second: "))
+
+sum = first + second
+
+print("sum= "+str(sum))
+
+course = 'Python for Beginners'
+print(course.upper())
+print(course.lower())
